e32e1cb13bfa68af9b1501ac31fd6ef13b9781d6
KrishnaI2424 <krishnabiyer@hotmail.com>
2024-07-02 12:00:53 -0700

    character

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -6,8 +6,7 @@ var vel : Vector2=Vector2()
 @onready var sprite : Sprite2D =get_node("Icon")
 
 func _process(delta):
-	if input_event:
-		position += Vector2(5, 0)
+	position += Vector2(5, 0)
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
