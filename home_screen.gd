875bba7427dd945ff70f1a04225383f58a8c8175
dwu2617 <dwu@eastsideprep.org>
2024-07-02 13:52:42 -0700

    enter title

---
+++ home_screen.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var title = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ($starting_prompts.CTT_title != null && !title):
+		$CTT_title.text = $starting_prompts.CTT_title
+		title = true
