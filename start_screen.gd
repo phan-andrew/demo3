61a5f61eef402680d056f0fca4e910f1acffbdd1
dwu2617 <dwu@eastsideprep.org>
2024-07-02 08:46:19 -0700

    Added basic starting buttons to start screen which links to home screen

---
+++ start_screen.gd
@@ -0,0 +1,17 @@
+extends Node2D
+var home = preload("res://home_screen.tscn").instantiate()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_3_pressed():
+	get_tree().get_root().add_child(home)
+	hide ()
+
