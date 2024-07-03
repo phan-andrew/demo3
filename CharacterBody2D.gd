0866086323e3c503276dc77062620ea94dadc33d
dwu2617 <dwu@eastsideprep.org>
2024-07-03 08:02:56 -0700

    dropdown scene added

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -5,12 +5,12 @@ var vel : Vector2=Vector2()
 @onready var sprite : Sprite2D =get_node("Icon")
 
 func _process(delta):
-	if Input.is_action_pressed("move_right"):
-		move=true
-	if move:
-		position += Vector2(3, 0)
-		modulate = Color8(40,183,215)
-	
+	#if Input.is_action_pressed("move_right"):
+		#move=true
+	#if move:
+		#position += Vector2(3, 0)
+		#modulate = Color8(40,183,215)
+	pass
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
