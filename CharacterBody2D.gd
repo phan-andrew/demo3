e1b3103cc7fff44976abd4517066729475e3f9f4
KrishnaI2424 <krishnabiyer@hotmail.com>
2024-07-03 09:15:35 -0700

    changes

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 var move=false
 var vel : Vector2=Vector2()
-@onready var sprite : Sprite2D =get_node("Icon")
+@onready var sprite : Sprite2D =get_node("Pixil-frame-0")
 
-func _process(delta):
+func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		move=true
 	if move:
-		position += Vector2(3, 0)
+	
 		modulate = Color8(40,183,215)
 	
 # Called when the node enters the scene tree for the first time.
