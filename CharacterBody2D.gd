e1620ec254bb9a3e9b0c8e405d0c96db994123b5
KrishnaI2424 <krishnabiyer@hotmail.com>
2024-07-03 10:01:40 -0700

    Card collision

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -8,7 +8,10 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		move=true
 	if move:
-		move_and_collide(Vector2(1,0))
+		move_and_collide(Vector2(5,0))
+		var collision := move_and_collide(Vector2(5,0))
+		if collision != null:
+			print("Collided with: ")
 		modulate = Color8(40,183,215)
 
 func _ready():
