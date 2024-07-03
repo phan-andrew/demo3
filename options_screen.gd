0b6d3119c66420ad18c046b7cb664e2c404fdc50
Ian <ianlarson1106@gmail.com>
2024-07-03 10:46:35 -0700

    t

---
+++ options_screen.gd
@@ -0,0 +1,15 @@
+extends Control
+@export
+var bus_name = "Master"
+var bus_index = 0
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
