52ad5e15f3044e1c60a7a054a45a0eb832004865
KrishnaI2424 <krishnabiyer@hotmail.com>
2024-07-03 11:27:48 -0700

    animations for now

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -11,8 +11,7 @@ func _physics_process(delta):
 		move_and_collide(Vector2(5,0))
 		var collision := move_and_collide(Vector2(5,0))
 		if collision != null:
-			print("Collided with: ")
-		modulate = Color8(40,183,215)
+			modulate = Color8(40,183,215)
 
 func _ready():
 	pass 
