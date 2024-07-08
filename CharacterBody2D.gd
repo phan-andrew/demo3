f75481d719028ad6d31a914c4e6a251e9324f29c
KrishnaI2424 <krishnabiyer@hotmail.com>
2024-07-08 10:47:45 -0700

    now resets =)

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -4,6 +4,9 @@ var move=false
 var vel : Vector2=Vector2()
 @onready var sprite : Sprite2D =get_node("Pixil-frame-0")
 
+#func wait(seconds: float) -> void:
+  #await get_tree().create_timer(seconds).timeout
+
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		move=true
@@ -12,8 +15,10 @@ func _physics_process(delta):
 		var collision := move_and_collide(Vector2(5,0))
 		if collision != null:
 			modulate = Color8(40,183,215)
-			print("collided")
-			position.x=0
-			position.y=0
+			if Input.is_action_pressed("reset"):
+				position.x=0
+				position.y=0
+				move=false
+
 func _ready():
 	pass 
