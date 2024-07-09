a2371a191e7b3f5363a96fb92977fd9956e64eff
Monish Madhu <monishmadhu86@gmail.com>
2024-07-09 09:28:23 -0700

    Dropdown options for both

--- dropdown.gd
+++ dropdown.gd
@@ -7,7 +7,9 @@ var generateDCard = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	import_resources_data()
+	add_attack_options()
+	add_defend_options()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,6 +24,36 @@ func _on_attack_option_item_selected(index):
 func _on_defend_option_item_selected(index):
 	defend_choice = index
 	generateDCard = true
-	
 
+
+var attack_dict={}
+var defend_dict={}
+
+func import_resources_data():
+	var file= FileAccess.open("res://data/ATT&CK_Names.txt", FileAccess.READ)
+	while !file.eof_reached():
+		var data_set = Array(file.get_csv_line())
+		attack_dict[attack_dict.size()] = data_set
+	file.close()
 	
+	var file2= FileAccess.open("res://data/D3FEND_Names.txt", FileAccess.READ)
+	while !file2.eof_reached():
+		var data_set2 = Array(file2.get_csv_line())
+		defend_dict[defend_dict.size()] = data_set2
+	file2.close()
+	
+func add_attack_options():
+	var drop = $attack_option
+	var a = 0
+	while a<attack_dict.size():
+		drop.add_item(attack_dict[a][0])
+		a+=1
+	drop.select(-1)
+	
+func add_defend_options():
+	var drop = $defend_option
+	var a = 0
+	while a<defend_dict.size():
+		drop.add_item(defend_dict[a][0])
+		a+=1
+	drop.select(-1)
