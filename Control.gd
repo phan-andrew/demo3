a89c043284740deefd12c0fc5a7c86d5ad79b119
Monish Madhu <monishmadhu86@gmail.com>
2024-07-09 09:51:57 -0700

    fixing help screen

--- Control.gd
+++ Control.gd
@@ -1,18 +1,8 @@
 extends Control
 
-
-var example_dict={}
-
 func _ready():
-	import_resources_data()
+	pass
 
-func import_resources_data():
-	var file= FileAccess.open("res://data/ATT&CK.txt", FileAccess.READ)
-	while !file.eof_reached():
-		var data_set = Array(file.get_csv_line())
-		example_dict[example_dict.size()] = data_set
-	file.close()
-	var a=0
-	while a<5:
-		print(example_dict[a][0])
-		a+=1
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://start_screen.tscn")
+	hide()
