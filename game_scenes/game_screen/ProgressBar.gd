65dda146c9ca86759a6d1bbfb383c395fb2eb168
Ian <ianlarson1106@gmail.com>
2024-07-12 10:44:24 -0700

    l

--- game_scenes/game_screen/ProgressBar.gd
+++ game_scenes/game_screen/ProgressBar.gd
@@ -4,7 +4,7 @@ var total_segments = 0
 var total_time = 0
 var current_point = 0
 var index = 2 
-var current_time = 0
+var current_time = 1
 
 func _ready():
 	total_segments = str_to_var(Mitre.timeline_dict[0][0])
@@ -18,8 +18,8 @@ func _on_submit_pressed():
 
 func _progress():
 	if (current_point < total_segments):
-		current_point + 1
-		index + 1
-		current_time = Mitre.timeline_dict[index][1]
-	percent = (current_time/total_time)*100
+		current_point = current_point + 1
+		index = index + 1
+		current_time = str_to_var(Mitre.timeline_dict[index][1])
+	percent = percent + ((float(current_time)/total_time)*100)
 	value = percent
