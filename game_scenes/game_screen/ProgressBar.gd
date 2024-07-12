ab2ed2ccae09be8b40b4cae78821cc0cd7a68690
Ian <ianlarson1106@gmail.com>
2024-07-12 09:17:03 -0700

    h

--- game_scenes/game_screen/ProgressBar.gd
+++ game_scenes/game_screen/ProgressBar.gd
@@ -1,20 +1,24 @@
 extends ProgressBar
 var percent = 0
+var total_segments = 0
+var total_time = 0
+var current_point = 0
+var curent_time = 0
 
 func _ready():
 	pass
+	# print(_load())
 
 func _process(delta):
 	pass
 
 func _load():
-	var file = FileAccess.open("res://Test_progressbar.txt", FileAccess.READ)
+	var file = FileAccess.open("res://Test_progressbar2.txt", FileAccess.READ)
 	var content = file.get_as_text()
 	return content
 
 func _on_button_pressed():
 	_progress()
-
 
 func _progress():
 	percent = 15
