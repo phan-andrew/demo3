a3d5fecf0066ff2ceef561bc8ac6865c79cc656e
Monish Madhu <monishmadhu86@gmail.com>
2024-07-18 14:18:19 -0700

    mission input scene

--- game_scenes/mission_input_screen/mission_inputs.gd
+++ game_scenes/mission_input_screen/mission_inputs.gd
@@ -1,5 +1,6 @@
 extends Node2D
-
+var red_mission
+var blue_mission
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +20,11 @@ func _on_button_pressed():
 	Settings.changed_scene = 1
 	get_tree().change_scene_to_file("res://game_scenes/start_screen/start_screen.tscn")
 	hide ()
+
+func _on_button_2_pressed():
+	red_mission = $TextEdit.text
+	$TextEdit.clear()
+
+func _on_button_3_pressed():
+	blue_mission = $TextEdit2.text
+	$TextEdit2.clear()
