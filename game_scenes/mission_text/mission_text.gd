fd5c1ea85b4adab3a13fdb693d5a59096b75c4d1
Ian <ianlarson1106@gmail.com>
2024-07-18 13:19:48 -0700

    h

--- game_scenes/mission_text/mission_text.gd
+++ game_scenes/mission_text/mission_text.gd
@@ -1,13 +1,9 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	if Settings.changed_scene == 1:
 		$mouse_click.playing = true
 		Settings.changed_scene = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
