83638855340d6a54e704b5445d5fecd95b4f312e
dwu2617 <dwu@eastsideprep.org>
2024-07-30 10:42:36 -0700

    change color for theme screen

--- game_scenes/options_screen/options_screen.gd
+++ game_scenes/options_screen/options_screen.gd
@@ -12,6 +12,9 @@ func _ready():
 	bus2_index = AudioServer.get_bus_index(bus2_name)
 	$HSlider2.value = Settings.sound_effects
 	$Theme_select.selected = Settings.theme
+	$theme.stream = preload("res://audio/themes/sea.mp3")
+	$theme.play()
+	Color(0.0, 0.25, 0.5)
 
 func _process(delta):
 	pass
@@ -27,6 +30,20 @@ func _on_h_slider_value_changed(value):
 
 func _on_theme_select_item_selected(index):
 	Settings.theme = index
+	
+	if index == 0:
+		$theme.stream = preload("res://audio/themes/sea.mp3")
+		$theme.volume_db = 0
+		$bg.modulate = Color(0.0, 0.25, 0.5)
+	if index == 1:
+		$theme.stream = preload("res://audio/themes/air.mp3")
+		$theme.volume_db = 2
+		$bg.modulate = Color(0.146, 0.372, 0.959)
+	if index == 2:
+		$theme.stream = preload("res://audio/themes/land.mp3")
+		$theme.volume_db = 0
+		$bg.modulate = Color(0.552, 0.415, 0.161)
+	$theme.play()
 
 func _on_h_slider_2_value_changed(value):
 	Settings.sound_effects = value
