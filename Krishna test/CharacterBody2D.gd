941b293aeb45a8b4d5abcc5c6e6b15d79a967ca6
KrishnaI2424 <krishnabiyer@hotmail.com>
2024-07-30 09:43:18 -0700

    made a back button for the easter egg

--- Krishna test/CharacterBody2D.gd
+++ Krishna test/CharacterBody2D.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-var move=false
+var move=true
+var vectorx=7
+var vectory=7
 var vel : Vector2=Vector2()
 @onready var sprite : Sprite2D =get_node("Pixil-frame-0")
 
@@ -8,17 +10,30 @@ var vel : Vector2=Vector2()
   #await get_tree().create_timer(seconds).timeout
 
 func _physics_process(delta):
-	if Input.is_action_pressed("move_right"):
-		move=true
+	
 	if move:
-		move_and_collide(Vector2(5,0))
-		var collision := move_and_collide(Vector2(5,0))
+		move_and_collide(Vector2(vectorx,vectory))
+		var collision := move_and_collide(Vector2(vectorx,vectory))
 		if collision != null:
+			var rng=RandomNumberGenerator.new()
 			modulate = Color8(40,183,215)
-			if Input.is_action_pressed("reset"):
-				position.x=0
-				position.y=0
-				move=false
+			if rng.randi_range(0,1)==0:
+				vectorx=-vectorx
+			if rng.randi_range(0,1)==0:
+				vectory=-vectory
+			if rng.randi_range(0,3)==2:
+				vectorx+=1
+				vectory+=1
+			elif rng.randi_range(0,3)==1:
+				vectorx-=1
+				vectory-=1
+			
+			
 
 func _ready():
 	pass 
+
+
+	
+func _on_mouse_entered():
+	get_tree().change_scene_to_file("res://game_scenes/help_screen/help_screen.tscn")
