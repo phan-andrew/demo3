554e1af742fd579c731fbfaff440a6764a9423a1
gillian-morris <gamchihuahua@gmail.com>
2025-06-13 14:14:14 -0700

    remove control

--- game_scenes/game_screen/Timer_Label2.gd
+++ game_scenes/game_screen/Timer_Label2.gd
@@ -8,7 +8,6 @@ var play = false
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if startTimer && play:
